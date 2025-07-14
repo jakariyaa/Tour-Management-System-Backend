edc2c98c00df2d8c460da92f49bad706c9201164
jakariyaa <jakariya3460@gmail.com>
2025-07-15 00:41:55 +0600

    implement UserService for user creation and refactor createUser in UserController

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,11 +1,10 @@
 import { Request, Response } from "express";
 import { constants } from "http2";
-import { User } from "./user.model";
+import { UserService } from "./user.service";
 
 const createUser = async (req: Request, res: Response) => {
   try {
-    const { name, email } = req.body;
-    const user = await User.create({ name, email });
+    const user = await UserService.createUser(req.body);
     res.status(constants.HTTP_STATUS_CREATED).json({
       success: true,
       message: "User created successfully",
