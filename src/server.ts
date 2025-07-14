8020ac3703a958f9edf63f74ccdfedb382370b81
jakariyaa <jakariya3460@gmail.com>
2025-07-14 18:36:38 +0600

    setup finished

---
+++ src/server.ts
@@ -0,0 +1,73 @@
+/* eslint-disable no-console */
+import "dotenv/config";
+import { Server } from "http";
+import mongoose from "mongoose";
+import app from "./app";
+import { env } from "./app/config/env";
+
+let server: Server;
+
+const startServer = async () => {
+  try {
+    const result = await mongoose.connect(env.MONGO_URI);
+    console.log("Connected to MongoDB:", result.connection.name);
+
+    server = app.listen(env.PORT, () => {
+      console.log("Server started on port", env.PORT);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
+
+process.on("SIGINT", () => {
+  console.log("Received SIGINT signal. Server shutting down...");
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+});
+
+process.on("SIGTERM", () => {
+  console.log("Received SIGTERM signal. Server shutting down...");
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+});
+
+process.on("unhandledRejection", (error) => {
+  console.log("Server shutting down...");
+  console.log("Unhandled Rejection Error Detected... \n", error);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+});
+
+//Promise.reject(new Error("Unhandled Rejection Error Provided"));
+
+process.on("uncaughtException", (error) => {
+  console.log("Server shutting down...");
+  console.log("Uncaught Exception Error Detected... \n", error);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+});
+
+//throw new Error("Uncaught Exception Error Provided");
