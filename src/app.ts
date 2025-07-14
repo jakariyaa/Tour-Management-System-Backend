2abf9613c0a8cbbb811e62974f105d5df8ad59eb
jakariyaa <jakariya3460@gmail.com>
2025-07-15 00:27:39 +0600

    refactor routing structure to use centralized RoutesHandler for user routes

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
 import cors from "cors";
 import express, { Application } from "express";
-import { UserRoutes } from "./app/modules/user/user.route";
+import { RoutesHandler } from "./app/routes";
 
 const app: Application = express();
 
 app.use(express.json());
 app.use(cors({ origin: "http://localhost:5173" }));
 
-app.use("/api/v1/users", UserRoutes);
+app.use("/api/v1", RoutesHandler);
 
 export default app;
