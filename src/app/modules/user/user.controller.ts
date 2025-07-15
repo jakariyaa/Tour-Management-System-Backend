f4c4d11adbde6b9fc58667e581850193453f9864
jakariyaa <jakariya3460@gmail.com>
2025-07-15 13:38:36 +0600

    refactor UserController to use sendResponse utility for consistent response formatting

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,11 +1,13 @@
 import { Request, Response } from "express";
 import { constants } from "http2";
 import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
 import { UserService } from "./user.service";
 
 const createUser = catchAsync(async (req: Request, res: Response) => {
   const user = await UserService.createUser(req.body);
-  res.status(constants.HTTP_STATUS_CREATED).json({
+  sendResponse(res, {
+    statusCode: constants.HTTP_STATUS_CREATED,
     success: true,
     message: "User created successfully",
     data: user,
@@ -14,10 +16,12 @@ const createUser = catchAsync(async (req: Request, res: Response) => {
 
 const getAllUsers = catchAsync(async (req: Request, res: Response) => {
   const users = await UserService.getAllUsers();
-  res.status(constants.HTTP_STATUS_OK).json({
+  sendResponse(res, {
+    statusCode: constants.HTTP_STATUS_OK,
     success: true,
-    message: "Users fetched successfully",
+    message: "Users retrieved successfully",
     data: users,
+    meta: { total: users.length },
   });
 });
 
